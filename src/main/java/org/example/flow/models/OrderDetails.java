46c601cd73a5b21ad02178cac7eba011150428b6
pham-van-man <phamvanman.com.vn@gmail.com>
2025-03-21 14:00:28 +0700

    first commit

--- src/main/java/org/example/flow/models/OrderDetails.java
+++ src/main/java/org/example/flow/models/OrderDetails.java
@@ -1,15 +1,17 @@
 package org.example.flow.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
 import org.hibernate.validator.constraints.Range;
 
+@Entity
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(uniqueConstraints = @jakarta.persistence.UniqueConstraint(columnNames = {"orderId", "productId"}))
-public class OrderDetails extends BaseEntity{
+public class OrderDetails extends BaseEntity {
     private Order order;
     private Product product;
     @Range(min = 0)
