888c55787427a30f52fb1e1738493cba24944d1d
cmtin1807 <cmtin1807@gmail.com>
2025-03-24 16:48:39 +0700

    product

--- src/main/java/org/example/flow/repositories/ProductRepository.java
+++ src/main/java/org/example/flow/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package org.example.flow.repositories;
 
+import org.example.flow.models.Category;
 import org.example.flow.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+    Page<Product> findAll(Pageable pageable);
 }
